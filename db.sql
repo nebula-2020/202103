f70926e334ba6c7d4392a9597dd07227310c4d6d
nebula-2020 <2781536086@qq.com>
2021-05-15 08:58:27 +0800

    feat:修改数据库
    修改数据库：增加一个字段长度以解决路径无法放入

--- db.sql
+++ db.sql
@@ -44,7 +44,7 @@ CREATE TABLE `user_details`  (
   `name` char(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '用户昵称',
   `ps` char(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '用户个性签名',
   `create_time` datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP(0),
-  `icon` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '用户头像路径',
+  `icon` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '用户头像路径',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
